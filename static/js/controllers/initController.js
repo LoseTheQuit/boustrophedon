b27d655cc545fa4d06f3be0df7648703d4a01c51
Brendan Carr <Brendan Carr>
2016-04-19 19:31:33 -0400

    update - 004

--- static/js/controllers/initController.js
+++ static/js/controllers/initController.js
@@ -6,7 +6,7 @@ angular.module("initModule")
     .controller('initController', function ($scope, dataService) {
 
 
-        $scope.instagramDataLink = 'https://www.instagram.com/oauth/authorize?client_id=b23670e220f14f1c89c11f627c9f9953&redirect_uri=https://the-mixup.herokuapp.com&response_type=code';
+        $scope.instagramDataLink = 'https://www.instagram.com/oauth/authorize?client_id=b23670e220f14f1c89c11f627c9f9953&redirect_uri=https://127.0.0.1&response_type=code';
 
         console.log("INSIDE initController");
 
